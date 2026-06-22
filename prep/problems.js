2a148f66658d80c36c356b00d4c6d5dff6629ac2
Shahriar <s.ahmed.ldn@gmail.com>
2026-06-22 14:58:30 +0100

    created file to troubleshoot exercises

---
+++ prep/problems.js
@@ -0,0 +1,18 @@
+// Write a function that will calculate the area of a rectangle
+//   given it's width and height
+
+let width = 3; // This is just a variable
+let height = 4; // This is also a variable
+
+function calculateArea() { // This is a function to calculate the area
+  const area = width * height;
+  console.log('hello world')
+}
+
+calculateArea
+
+console.log(area); //This displays the value 
+
+// The code comes up with an error because the variable 
+// is within the function so it does not exist in the global scope
+
