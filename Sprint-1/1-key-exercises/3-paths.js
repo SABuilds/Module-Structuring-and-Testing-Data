2f45413e837fa4ca4e24d29a5de5fafa8428f553
Shahriar <s.ahmed.ldn@gmail.com>
2026-06-22 17:06:06 +0100

    added slice

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,7 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
+const dir = filePath.slice();
 const ext = ;
 
 // https://www.google.com/search?q=slice+mdn
