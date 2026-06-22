98632879e6756f1bfa509b33b8690c4cd8d8518e
Shahriar <s.ahmed.ldn@gmail.com>
2026-06-22 14:58:10 +0100

    created functions for area and perimeter

--- prep/example.js
+++ prep/example.js
@@ -5,3 +5,19 @@ console.log("hello there!");
 function convertToPercentage(decimal) {
     return `${decimal * 100}%`
 }
+
+const height = 10;
+const width = 10; 
+
+const area = height * width;
+const perimeter = (height + height) + (width + width);
+console.log(area);
+console.log(perimeter);
+
+function areaofRectangle(high, wide) {
+    return high * wide; 
+}
+
+function perimeterofRectangle(height, width) {
+    return height + height + width + width;
+}
