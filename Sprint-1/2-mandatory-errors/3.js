9d17e06629db0c44ae51638bca1762b20f34e41a
Shahriar <s.ahmed.ldn@gmail.com>
2026-06-27 10:53:45 +0100

    Typed my prediction for the why the code is not working

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -4,6 +4,9 @@ const last4Digits = cardNumber.slice(-4);
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
 // Before running the code, make and explain a prediction about why the code won't work
+
+// I predict the reason why it won't work is because the slice can not use a negative integer
+
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
