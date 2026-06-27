1c9ac51e95ffdb4eed618d0ad3c08b23dce4a3cd
Shahriar <s.ahmed.ldn@gmail.com>
2026-06-27 11:18:29 +0100

    Corrected error by sandwiching numeric value within characters

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,6 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const Hour12ClockTime = "8:53pm";
+const Hour24ClockTime = "20:53";
+
+// error given when hovering over each of the variables is that an indentifier or keyword
+// can not follow immediately after a numeric value
+// To correct I just sandwich the numeric value within the characters
