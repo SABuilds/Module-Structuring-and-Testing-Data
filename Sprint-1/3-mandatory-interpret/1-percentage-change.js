e97cef88d8493babb1a9c2efbd1e140f7ddf319f
Shahriar <s.ahmed.ldn@gmail.com>
2026-06-27 11:34:33 +0100

    Answered the questions

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -13,10 +13,21 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
+// carprice.replaceAll(), Number(), priceAfterOneYear.replaceAll(), Number(), console.log()
+
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+
+//The error code was due a missing comma within priceAfterOneYear.replaceAll. 
 
 // c) Identify all the lines that are variable reassignment statements
 
+//carPrice on line 4 is a variable reassignment and so is priceAfterOneYear on line 5
+
 // d) Identify all the lines that are variable declarations
 
+//1, 2, 7, 8 
+
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+// This expression is finding all commas and replacing them as nothing. To put it simply, it is removing all commas
+// The purpose is to make the string into a pure numerical value so it can be used in calculations. 
