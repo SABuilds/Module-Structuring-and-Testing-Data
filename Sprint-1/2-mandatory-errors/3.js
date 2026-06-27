79d0da8cf5e2e8d0e8aebf92467d993e7c1cf1c8
Shahriar <s.ahmed.ldn@gmail.com>
2026-06-27 11:15:09 +0100

    corrected how cardNumber was defined to allow slice to work

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,7 @@
-const cardNumber = 4533787178994213;
+const cardNumber = '4533787178994213';
 const last4Digits = cardNumber.slice(-4);
+
+console.log(last4Digits);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -8,5 +10,15 @@ const last4Digits = cardNumber.slice(-4);
 // I predict the reason why it won't work is because the slice can not use a negative integer
 
 // Then run the code and see what error it gives.
+
+// The error I got was that cardNumber.slice is not a functions
+
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
+
+// I still believe the reason why this error is given is that a negative number is within the function parameter 
+// What is different is that the error given states that cardnumber.slice is not a function. Now that I think about it 
+// it could be that its because it is not defined as a string of characters. Usually characters should
+
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// I added '' to the string of numbers which then allowed the slice function to work as intended. 
