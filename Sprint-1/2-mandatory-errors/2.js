51aa7b9a7a78423285158cd78d57db9a6703c627
Shahriar <s.ahmed.ldn@gmail.com>
2026-06-27 10:52:01 +0100

    defined variable before console.log

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// The const should be defined before the console.log as the computer runs code line by line
+// Therefore it can't output using cityOfBirth as it only 'finds' out the variable after console.log
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
