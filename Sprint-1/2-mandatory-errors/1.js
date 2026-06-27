c439c1810e8e129b8460ad44fa69a91c88dd7dbb
Shahriar <s.ahmed.ldn@gmail.com>
2026-06-27 10:48:20 +0100

    changed const to let for the age variable

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,7 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
+// define age with let not const so it can be changed
 age = age + 1;
+
+console.log(age)
